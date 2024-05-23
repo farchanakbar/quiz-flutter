de359160dbe7ce64afe7f3e0e482b7001ae19346
Farchan Akbar <farchanakbar80@gmail.com>
2024-05-23 08:06:26 +0700

    first

---
+++ lib/quiz.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/data/questions.dart';
+import 'package:quiz/questions_screen.dart';
+import 'package:quiz/result_screen.dart';
+import 'package:quiz/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'result-screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(onSelectedAnswer: chooseAnswer);
+    } else if (activeScreen == 'result-screen') {
+      screenWidget = ResultScreen(
+        chosenAnswers: selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                end: Alignment.topLeft,
+                colors: [Colors.cyan, Colors.greenAccent],
+              ),
+            ),
+            child: screenWidget),
+      ),
+    );
+  }
+}
---
+++ lib/start_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(50, 219, 14, 14),
+          ),
+          const SizedBox(height: 80),
+          Text(
+            'Learn Flutter The Fun Way!',
+            style: GoogleFonts.lato(
+              fontSize: 20,
+              color: const Color.fromARGB(255, 146, 43, 36),
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+              onPressed: startQuiz,
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              icon: const Icon(
+                Icons.arrow_right_alt,
+                size: 30,
+              ),
+              label: const Text('Start Quiz')),
+        ],
+      ),
+    );
+  }
+}
